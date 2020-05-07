c7e8a4e4464560ec2b8a6425b6f825ad28cb53fa
Илатовский Александр Геннадьевич <AIlatovskiy@alfabank.ru>
2020-05-07 17:58:32 +0300

    resolvers

---
+++ data.ts
@@ -0,0 +1,52 @@
+export const accounts = [
+	{
+		id: 'm0001',
+		name: 'Основной'
+	},
+	{
+		id: 'q0002',
+		name: 'На случай карантина'
+	},
+	{
+		id: 'b0003',
+		name: 'Заначка на черный день'
+	}
+]
+
+export const categories = [
+	{
+		id: 'ot0001',
+		name: 'Запланированные расходы'
+	},
+	{
+		id: 'os0002',
+		name: 'Незапланированные расходы'
+	},
+	{
+		id: 'is0003',
+		name: 'Зарплата'
+	},
+	{
+		id: 'ij0004',
+		name: 'В куртке нашел'
+	},
+]
+
+export const transactions = [
+	{
+		id: '0001',
+		account: 'm0001',
+		direction: 'INCOMING',
+		timestamp: 'Thu, 07 May 2020 14:36:13 GMT',
+		category: 'is0003',
+		amount: 100000.33
+	},
+	{
+		id: '0002',
+		account: 'm0001',
+		direction: 'OUTCOMING',
+		timestamp: 'Thu, 07 May 2020 14:49:26 GMT',
+		category: 'ot0001',
+		amount: 5600
+	}
+]
--- index.ts
+++ index.ts
@@ -1,12 +1,45 @@
 import { ApolloServer, gql } from 'apollo-server';
 import glob from 'glob';
+import { DateTimeResolver } from 'graphql-scalars';
 import { readFileSync } from 'fs';
+import { accounts, categories, transactions } from './data';
 
-const sdls = glob.sync('./sdl/**/*.graphql');
+const typeDefs = glob.sync('./sdl/**/*.graphql').map(path => readFileSync(path, 'utf-8'));
 
+const resolvers = {
+	Query: {
+		account: () => ({}),
+		category: () => ({}),
+		transaction: () => ({}),
+	},
+	AccountQuery: {
+		all: () => accounts
+	},
+	CategoryQuery: {
+		all: () => categories
+	},
+	TransactionQuery: {
+		all: () => transactions
+	},
+	Transaction: {
+		account: (parent: any) => accounts.find(account => account.id === parent.account),
+		category: (parent: any) => categories.find(category => category.id === parent.category)
+	},
+	Account: {
+		amount: (parent: any) => transactions
+			.filter(transaction => transaction.account === parent.id)
+			.reduce(
+				(result, transaction) => (
+					result + transaction.amount * (transaction.direction === 'OUTCOMING' ? -1 : 1)
+				), 0
+			)
+	},
+	DateTime: DateTimeResolver
+}
 
 const server = new ApolloServer({
-	typeDefs: sdls.map(path => readFileSync(path, 'utf-8'))
+	typeDefs,
+	resolvers
 });
 
 server.listen({ port: 8800 }).then(({ port }) => { console.log(`Running on ${port}`); });
