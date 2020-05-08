bb8a0ffb4797ac77cd6d926b87e9ac2cf1672fbc
Илатовский Александр Геннадьевич <AIlatovskiy@alfabank.ru>
2020-05-08 13:51:52 +0300

    add model types generating to codegen script

--- codegen.ts
+++ codegen.ts
@@ -1,5 +1,7 @@
 import glob from 'glob';
 import { readFileSync, writeFileSync } from 'fs';
+import { getFieldsWithDirectives, DirectiveUsage } from '@graphql-toolkit/common';
+import { unflatten } from 'flat';
 import { parse } from 'graphql';
 import { codegen } from '@graphql-codegen/core';
 import * as typescriptPlugin from '@graphql-codegen/typescript';
@@ -8,6 +10,39 @@ import * as typescriptResolversPlugin from '@graphql-codegen/typescript-resolver
 const typeDefs = glob.sync('./sdl/**/*.graphql').map(
 	path => readFileSync(path, 'utf-8')
 );
+
+const schema = parse(typeDefs.join('\n\n'));
+
+const fieldsWithModelExcludeDirective: Record<string, Record<string, DirectiveUsage[]>> = unflatten(Object.fromEntries(Object.entries(getFieldsWithDirectives(schema)).filter(
+	([fieldName, directiveUsages]) => directiveUsages.some(
+		directiveUsage => directiveUsage.name === 'excludeFieldFromModel' 
+	)
+))) || {};
+
+const fieldsWithReplaceFieldInModelDirective: Record<string, Record<string, DirectiveUsage[]>> = unflatten(Object.fromEntries(Object.entries(getFieldsWithDirectives(schema)).filter(
+	([fieldName, directiveUsages]) => directiveUsages.some(
+		directiveUsage => directiveUsage.name === 'replaceFieldInModel' 
+	)
+))) || {};
+
+const models = Object.keys({...fieldsWithModelExcludeDirective, ...fieldsWithReplaceFieldInModelDirective}).reduce(
+	(result, type) => {
+		const replacements = Object.entries(fieldsWithReplaceFieldInModelDirective[type] || {}).map(
+			([fieldName, directiveUsages]) => `${fieldName}: ${
+				directiveUsages.find(directiveUsage => directiveUsage.name === 'replaceFieldInModel')?.args['replacement']
+			}`
+		).join(';\n')
+		
+		return {
+			...result,
+			[type]: `type ${type}Model = Omit<${type}, ${
+				Object.keys({ ...fieldsWithModelExcludeDirective[type], ...fieldsWithReplaceFieldInModelDirective[type]}).map(
+					fieldName => `'${fieldName}'`
+				).join(' | ')
+			}>${replacements.length ? ` & { ${replacements} }`: ''}`
+		}
+	}, {})
+
 
 codegen({
 	filename: 'filename.ts',
@@ -24,6 +59,7 @@ codegen({
 				noSchemaStitching: true,
 				contextType: 'object',
 				avoidOptionals: true,
+				mappers: Object.keys(models).reduce((result, type) => ({...result, [type]: `${type}Model`}), {})
 			},
 		}
 	],
@@ -31,9 +67,9 @@ codegen({
 		typescript: typescriptPlugin,
 		typescriptResolvers: typescriptResolversPlugin,
 	},
-	schema: parse(typeDefs.join('\n\n')),	
+	schema
 }).then(typings => {
-	writeFileSync('./typings.ts', typings, { flag: 'w' });
+	writeFileSync('./typings.ts', [typings, Object.values(models).join(';\n')].join('\n\n'), { flag: 'w' });
 })
 
 
